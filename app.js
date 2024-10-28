c72a2bdbad5ce1029ba8f0126a141db7e61d433c
Max Salizar <maxsalizar@gmail.com>
2024-10-28 01:10:10 -0500

    using express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('todo aca')
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Ruta básica
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo desde Express!");
+});
+
+// Inicia el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
